af06b7d72b2a8f89acbc51227e23b8b23f6fd237
Lucas Ethan <asmrit1010@gmail.com>
2021-03-03 14:30:50 +0100

    Likes problem

--- likes.rb
+++ likes.rb
@@ -1,18 +1,15 @@
 def likes(names)
-
-  if names.empty?
-    puts "No one likes this."
-  end
-
-  if names.count == 1
-    puts "#{names.join(' ')} likes this."
-  end
-
-  if names.count == 2
-    puts "#{names.join(' and ')} like this."
-  end
-
-  if names.count == 3
-    puts "#{names.join(', ')} like this."
+  num_of_names = names.count
+  case num_of_names
+  when 0
+    "no one likes this"
+  when 1
+    "#{names[0]} likes this"
+  when 2
+    "#{names.join(' and ')} like this"
+  when 3
+    "#{names[0...-1].join(', ')} and #{names[-1]} like this"
+  else
+    "#{names[0..1].join(', ')} and #{num_of_names - 2} others like this"
   end
 end
