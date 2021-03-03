4fc2102f55e151682c34820d2145dd953e04092c
Lucas Ethan <asmrit1010@gmail.com>
2021-03-03 15:26:26 +0100

    Refactored credit card mask solution

--- 7kyu/maskify.rb
+++ 7kyu/maskify.rb
@@ -1,8 +1,6 @@
 def maskify(cc)
   length_of_cc = cc.length
-  if length_of_cc <= 4
-    cc
-  else
-    "#{'#' * (length_of_cc - 4)}#{cc[-4..-1]}"
-  end
+  return cc if length_of_cc <= 4
+
+  "#{'#' * (length_of_cc - 4)}#{cc[-4..-1]}"
 end
