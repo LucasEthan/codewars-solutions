9088071a7482344620fd51c6609a3627ad506c01
Lucas Ethan <asmrit1010@gmail.com>
2021-03-06 09:26:24 +0100

    Refactored create phone number solution

--- 6kyu/create_phone_number.rb
+++ 6kyu/create_phone_number.rb
@@ -1,7 +1,3 @@
 def create_phone_number(numbers)
-  area_code = numbers.first(3).join("")
-  phone_num_part_1 = numbers[3..5].join("")
-  phone_num_part_2 = numbers[6..9].join("")
-
-  "(#{area_code}) #{phone_num_part_1}-#{phone_num_part_2}"
+  format("(%d%d%d) %d%d%d-%d%d%d%d", *numbers)
 end
