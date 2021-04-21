771e18e1cc200a0c8af1d3d2690dcd08adb0064b
Lucas Ethan <asmrit1010@gmail.com>
2021-04-21 15:35:12 +0200

    Refactored pete the baker solution

--- 5kyu/pete_the_baker.rb
+++ 5kyu/pete_the_baker.rb
@@ -1,20 +1,3 @@
 def cakes(recipe, available)
-  num_cakes = 0
-  not_enough_ingredients = false
-  available_ingredients = available
-
-  loop do
-    recipe.each do |ingredient, units|
-      if available_ingredients.fetch(ingredient, 0) >= units
-        available_ingredients[ingredient] -= units
-      else
-        not_enough_ingredients = true
-      end
-    end
-
-    break if not_enough_ingredients
-
-    num_cakes += 1
-  end
-  num_cakes
+  recipe.collect { |ingredient, units| available.fetch(ingredient, 0) / units }.min
 end
