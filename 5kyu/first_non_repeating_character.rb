d7409a6d2562c1c0d50076fc6993e7dcb5757342
Lucas Ethan <asmrit1010@gmail.com>
2021-04-24 09:41:58 +0200

    Refactored first non repeating character solution

--- 5kyu/first_non_repeating_character.rb
+++ 5kyu/first_non_repeating_character.rb
@@ -1,9 +1,3 @@
 def first_non_repeating_letter(s)
-  unique_char = s.chars.detect { |character| s.count(character) == 1 }
-
-  if !unique_char.nil?
-    unique_char
-  else
-    ""
-  end
+  s.chars.detect { |character| s.upcase.count(character.upcase) == 1 } || ""
 end
